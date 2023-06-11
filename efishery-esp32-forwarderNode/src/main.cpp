c28c4e865778c4fb29234fb6d9f61428a74b7b94
ichsannurliyan <ichsan.nurliyan@gmail.com>
2023-06-11 21:03:52 +0700

    refactor Code

--- efishery-esp32-forwarderNode/src/main.cpp
+++ efishery-esp32-forwarderNode/src/main.cpp
@@ -17,6 +17,7 @@ boolean connected = false;
 const char * wifiSsid = "dlink-CCD0";
 const char * wifiPassword = "tpbrn33498";
 
+/* if defined Hive MQTT */
 // const char * mqttServer = "1e274e07458745289062e09df40ddbc0.s2.eu.hivemq.cloud";
 // int mqttPort = 8883;
 // String mqttUser = "efisheryassessment";
@@ -108,7 +109,6 @@ void loop() {
 
 void wifiInit() {
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(wifiSsid);
@@ -141,33 +141,50 @@ void callback(char* topic, byte* payload, unsigned int length)
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  String payloadString;
   for (int i=0;i<length;i++) {
+    payloadString  += (char)payload[i];
     Serial.print((char)payload[i]);
   }
   Serial.println();
+
+  StaticJsonDocument<96> doc;
+
+  DeserializationError error = deserializeJson(doc, payloadString);
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  const char* eventName = doc["eventName"];
+
+  if (eventName == "deviceReboot")
+  {
+    int reboot = doc["reboot"];
+    if (reboot == 1)
+    {
+      delay(5000);
+      ESP.restart();
+    }
+  }
 }
 
 void reconnect() 
 {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
     String clientId = "ESP32Client-";
     clientId += deviceId;
-    // Attempt to connect
     if (client.connect(clientId.c_str(),mqttUser.c_str(),mqttPassword.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      // client.publish("outTopic", "hello world");
-      // ... and resubscribe
       client.subscribe(mqttSubTopic.c_str());
     } else {
       wifiInit();
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -175,15 +192,13 @@ void reconnect()
 
 String pubMessageStruct()
 {
-  String message = "{\"eventName\":\"deviceStatus\",\"status\":[{\"device\":\"temperature\",\"value\":\"";
+  String message = "{\"eventName\":\"deviceStatus\",\"temperature\":\"";
   message += temperatureData; 
-  message += "\"},";
-  message += "{\"device\":\"humidity\",\"value\":\"";
+  message += "\",\"humidity\":\"";
   message += humidityData; 
-  message += "\"},";
-  message += "{\"device\":\"co2\",\"value\":\"";
+  message += "\",\"co2\":\"";
   message += co2Data; 
-  message += "\"}]}";
+  message += "\"}";
 
   return message;
 }
